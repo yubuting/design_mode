d520571e21b663dc3ce8ba3dc3d7699ff0d992a3
xiongtt <595321490@qq.com>
2019-09-04 00:55:11 +0800

    继承的理解

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 class Person {
-    constructor(name) {
+    constructor(name, age) {
         this.name = name
+        this.age = age
     }
     getName() {
         return this.name
     }
+    eat() {
+        alert(`${this.name} eat`)
+    }
+    speak() {
+        alert(`${this.name} speak`)
+    }
 }
-let p = new Person('beartaotao')
-alert(p.getName())
+// let p = new Person('beartaotao')
+// alert(p.eat())
+/**
+ * 继承 子类继承父类
+ */
+class Student extends Person {
+    constructor(name, age, number) {
+        super(name, age)
+        this.number = number
+    }
+    grade() {
+        alert(`${this.number}`)
+    }
+}
+
+let xiongtaotao = new Student('xiongtaotao', 27, 'A1')
+xiongtaotao.eat()
